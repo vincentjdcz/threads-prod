3306bcd8c2ee76f4a128dc0368ec9e713e42b0de
Vincent Dela Cruz <vincentjdelacruz@gmail.com>
2024-01-16 14:07:16 -0500

    implemented getting, creating, and deleting posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -34,5 +34,36 @@ const createPost = async (req, res) => {
     }
 };
 
-export { createPost };
+const getPost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        res.status(200).json({ post });
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        if (post.postedBy.toString() !== req.user._id.toString()){
+            return res.status(401).json({message: "Unauthorized to delete post"});
+        }
+
+        await Post.findByIdAndDelete(req.params.id); //delete the post
+        res.status(200).json({ message: "Post deleted successfully" });
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
+
+export { createPost, getPost, deletePost };
 
